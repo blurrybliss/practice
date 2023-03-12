9d52d850131d1a8be5c9ce3c4db7dffb0c6a4880
blurrybliss <82766730+blurrybliss@users.noreply.github.com>
2023-03-12 20:23:29 +0300

    Create rational.h

---
+++ tasks/3rdpractice/2ndtask/rational.h
@@ -0,0 +1,15 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+
+class rational {
+public:
+    rational(int a1 = 0, int b1 = 1);
+    void set(int a1, int b1);
+    void show() const;
+private:
+    int a, b;
+    void reduce();
+};
+
+#endif 
